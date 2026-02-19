450b0eb0a139f56b5f12c13b74e0467b6dd8947f
Himanshu9097 <himanshu748808@gmail.com>
2026-02-19 22:10:31 +0530

    added the check route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"Welcome to the Music App API"
+    })
+})
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/music',musicRoutes);
